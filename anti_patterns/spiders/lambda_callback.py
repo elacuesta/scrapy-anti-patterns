fd91ccd683b73c5de9628acdbc297ac37fec2b4c
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2018-06-18 22:26:32 -0300

    Lambda callback: add good example

--- anti_patterns/spiders/lambda_callback.py
+++ anti_patterns/spiders/lambda_callback.py
@@ -2,13 +2,13 @@
 import scrapy
 
 
-class LambdaCallbackSpider(scrapy.Spider):
-    name = 'lambda-callback'
+class LambdaCallbackBadSpider(scrapy.Spider):
+    name = 'lambda-callback-bad'
     start_urls = ['https://example.org/']
     custom_settings = {
         'LOG_LEVEL': 'DEBUG',
         'SCHEDULER_DEBUG': True,
-        'JOBDIR': 'lambda-callback-temp',
+        'JOBDIR': 'lambda-callback-bad-temp',
     }
 
     def parse(self, response):
@@ -17,5 +17,25 @@ class LambdaCallbackSpider(scrapy.Spider):
         return scrapy.Request('https://example.com', callback=callback)
 
     def parse_additional(self, response, data):
+        data.append(response.url)
+        return {'data': data}
+
+
+class LambdaCallbackGoodSpider(scrapy.Spider):
+    name = 'lambda-callback-good'
+    start_urls = ['https://example.org/']
+    custom_settings = {
+        'LOG_LEVEL': 'DEBUG',
+        'SCHEDULER_DEBUG': True,
+        'JOBDIR': 'lambda-callback-good-temp',
+    }
+
+    def parse(self, response):
+        data = [response.url]
+        return scrapy.Request('https://example.com', meta={'data': data},
+                              callback=self.parse_additional)
+
+    def parse_additional(self, response):
+        data = response.meta['data']
         data.append(response.url)
         return {'data': data}
